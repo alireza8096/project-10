a4e9dfc3dcb628d45180e07a9d102ec133c285e4
yasamansamadzadeh <y.samadzadeh@yahoo.com>
2019-05-02 07:56:42 +0430

    add buff fields and some methods

--- model/GraveYard.java
+++ model/GraveYard.java
@@ -22,6 +22,4 @@ public class GraveYard {
         this.graveYardIsBeingUsed = graveYardIsBeingUsed;
     }
 
-
-
 }
--- model/collection/Buff.java
+++ model/collection/Buff.java
@@ -1,4 +1,29 @@
 package model.collection;
 
 public class Buff {
+    private int turnsLeft;
+
+    public int getTurnsLeft() {
+        return turnsLeft;
+    }
+
+    public void setTurnsLeft(int turnsLeft) {
+        this.turnsLeft = turnsLeft;
+    }
+
+    public void weaknessBuff()
+    {
+
+    }
+
+    public void stunBuff()
+    {
+
+    }
+
+    public void disarmBuff()
+    {
+
+    }
+
 }
