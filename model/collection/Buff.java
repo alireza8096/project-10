3c2f911add317c4854b3e78298d883fc38d778dd
yasamansamadzadeh <y.samadzadeh@yahoo.com>
2019-05-02 08:32:41 +0430

    add buff fields

--- model/collection/Buff.java
+++ model/collection/Buff.java
@@ -1,29 +1,40 @@
 package model.collection;
 
 public class Buff {
-    private int turnsLeft;
+    private int forHowManyTurns;
+    private String name;
+    private String type;
+    private int howMuchImpact;
 
-    public int getTurnsLeft() {
-        return turnsLeft;
+    public int getForHowManyTurns() {
+        return forHowManyTurns;
     }
 
-    public void setTurnsLeft(int turnsLeft) {
-        this.turnsLeft = turnsLeft;
+    public void setForHowManyTurns(int forHowManyTurns) {
+        this.forHowManyTurns = forHowManyTurns;
     }
 
-    public void weaknessBuff()
-    {
-
+    public String getName() {
+        return name;
     }
 
-    public void stunBuff()
-    {
-
+    public void setName(String name) {
+        this.name = name;
     }
 
-    public void disarmBuff()
-    {
-
+    public String getType() {
+        return type;
     }
 
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getHowMuchImpact() {
+        return howMuchImpact;
+    }
+
+    public void setHowMuchImpact(int howMuchImpact) {
+        this.howMuchImpact = howMuchImpact;
+    }
 }
