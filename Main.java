90f39a84ab0bede943e89b15693c5a738e4ebbc9
bahar <baharkh127@gmail.com>
2019-05-03 16:09:53 +0430

    added a method in Hand class for inserting a card from hand in map

--- Main.java
+++ Main.java
@@ -4,10 +4,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        Scanner scanner = new Scanner(System.in);
-        Controller.createAll();
-        while (true){
-            Controller.handleCommands(scanner);
-        }
+
     }
 }
